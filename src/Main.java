c37b84fdb6b2ab842cfc426e9599c09b172a1ea6
Riccardo Carraro <riccardo.carraro.7@studenti.unipd.it>
2018-04-18 15:20:29 +0200

    lock file

--- src/Main.java
+++ src/Main.java
@@ -20,7 +20,7 @@ public class Main {
         {
             myNode = new Kademlia();
         }
-        catch(AlreadyInstancedException aie)
+        catch (AlreadyInstancedException aie)
         {
             System.exit(1);
         }
@@ -36,145 +36,153 @@ public class Main {
 
         while (keep)
         {
-            System.out.print("KadNode@ " + myNode.getNodeID() + "-: ");
-            in = reader.nextLine();
-            String[] split = in.split(" ");
-            switch (split[0])
+            try
             {
-                case "get":
-                    //Cerca di ottenere un file di cui conosce l'esistenza, se è possibile
-                    if (split.length > 1)
-                    {
-                        synchronized (myNode.getFileList())
+                System.out.print("KadNode@ " + myNode.getNodeID() + "-: ");
+                in = reader.nextLine();
+                String[] split = in.split(" ");
+                switch (split[0])
+                {
+                    case "get":
+                        //Cerca di ottenere un file di cui conosce l'esistenza, se è possibile
+                        if (split.length > 1)
                         {
-                            for (KadFile i : myNode.getFileList())
+                            synchronized (myNode.getFileList())
                             {
-                                if (i.getFileName().equals(split[1]))
+                                for (KadFile i : myNode.getFileList())
                                 {
-                                    myNode.findValue(i.getFileID(), true);
-                                    break;
+                                    if (i.getFileName().equals(split[1]))
+                                    {
+                                        myNode.findValue(i.getFileID(), true);
+                                        break;
+                                    }
                                 }
-                            }
-                        }
-                    }
-                    else
-                    {
-                        System.out.println("Please define a file to get");
-                    }
-                    break;
-                case "ls":
-                    //Lista tutti i file di cui conosco l'esistenza
-                    for (KadFile i : myNode.getFileList())
-                    {
-                        System.out.println(i.toString());
-                    }
-                    break;
-                case "ip":
-                    System.out.println(myNode.getIP().getHostAddress());
-                    break;
-                case "store":
-                    if (split.length > 1)
-                    {
-                        try
-                        {
-                            myNode.store(split[1]);
-                        }
-                        catch (IOException | InvalidParameterException ioe)
-                        {
-                            System.out.println("File specificato non valido");
-                        }
-                    }
-                    else
-                    {
-                        System.out.println("Inserisci il percorso del file");
-                    }
-                    break;
-                case "findnode":
-                    if (split.length > 1)
-                    {
-                        List<KadNode> l = myNode.findNode(new BigInteger(split[1]));
-                        for (int i = 0; i < l.size(); i++)
-                        {
-                            System.out.println("******* " + l.get(i));
-                        }
-                    }
-                    else
-                    {
-                        System.out.println("Inserisci l'ID del nodo");
-                    }
-                    break;
-                case "delete":
-                    if (split.length > 1)
-                    {
-                        try
-                        {
-                            myNode.delete(new BigInteger(split[1]));
-                        }
-                        catch (FileNotKnownException fnk)
-                        {
-                            System.out.println("Il file non esiste o non ne sei il proprietario");
-                        }
-                    }
-                    else
-                    {
-                        System.out.println("Inserisci L'ID del file");
-                    }
-                    break;
-                case "ping":
-                    //Funzione di test
-                    if (split.length > 1)
-                    {
-                        try
-                        {
-                            if (myNode.ping(new KadNode(split[1], Short.parseShort(split[2]), new BigInteger(split[3]))))
-                            {
-                                System.out.println("VIVO");
-                            }
-                        }
-                        catch (NumberFormatException nfe)
-                        {
-                            if (myNode.ping(new KadNode(split[1], myNode.getPort(), new BigInteger(split[2]))))
-                            {
-                                System.out.println("VIVO");
-                            }
-                        }
-                    }
-                    else
-                    {
-                        System.out.println("Please define an IP address and ID or IP,Port and ID");
-                    }
-                    break;
-                case "findvalue":
-                    if (split.length > 1)
-                    {
-                        Object value = myNode.findValue(new BigInteger(split[1]), false);
-                        if (value instanceof List)
-                        {
-                            for (int i = 0; i < ((List<KadNode>) value).size(); i++)
-                            {
-                                System.out.println("******* " + ((List<KadNode>) value).get(i));
                             }
                         }
                         else
                         {
-                            System.out.println("Il contenuto del file richiesto è stato trovato");
+                            System.out.println("Please define a file to get");
                         }
+                        break;
+                    case "ls":
+                        //Lista tutti i file di cui conosco l'esistenza
+                        for (KadFile i : myNode.getFileList())
+                        {
+                            System.out.println(i.toString());
+                        }
+                        break;
+                    case "ip":
+                        System.out.println(myNode.getIP().getHostAddress());
+                        break;
+                    case "store":
+                        if (split.length > 1)
+                        {
+                            try
+                            {
+                                myNode.store(split[1]);
+                            }
+                            catch (IOException | InvalidParameterException ioe)
+                            {
+                                System.out.println("File specificato non valido");
+                            }
+                        }
+                        else
+                        {
+                            System.out.println("Inserisci il percorso del file");
+                        }
+                        break;
+                    case "findnode":
+                        if (split.length > 1)
+                        {
+                            List<KadNode> l = myNode.findNode(new BigInteger(split[1]));
+                            for (int i = 0; i < l.size(); i++)
+                            {
+                                System.out.println("******* " + l.get(i));
+                            }
+                        }
+                        else
+                        {
+                            System.out.println("Inserisci l'ID del nodo");
+                        }
+                        break;
+                    case "delete":
+                        if (split.length > 1)
+                        {
+                            try
+                            {
+                                myNode.delete(new BigInteger(split[1]));
+                            }
+                            catch (FileNotKnownException fnk)
+                            {
+                                System.out.println("Il file non esiste o non ne sei il proprietario");
+                            }
+                        }
+                        else
+                        {
+                            System.out.println("Inserisci L'ID del file");
+                        }
+                        break;
+                    case "ping":
+                        //Funzione di test
+                        if (split.length > 1)
+                        {
+                            try
+                            {
+                                if (myNode.ping(new KadNode(split[1], Short.parseShort(split[2]), new BigInteger(split[3]))))
+                                {
+                                    System.out.println("VIVO");
+                                }
+                            }
+                            catch (NumberFormatException nfe)
+                            {
+                                if (myNode.ping(new KadNode(split[1], myNode.getPort(), new BigInteger(split[2]))))
+                                {
+                                    System.out.println("VIVO");
+                                }
+                            }
+                        }
+                        else
+                        {
+                            System.out.println("Please define an IP address and ID or IP,Port and ID");
+                        }
+                        break;
+                    case "findvalue":
+                        if (split.length > 1)
+                        {
+                            Object value = myNode.findValue(new BigInteger(split[1]), false);
+                            if (value instanceof List)
+                            {
+                                for (int i = 0; i < ((List<KadNode>) value).size(); i++)
+                                {
+                                    System.out.println("******* " + ((List<KadNode>) value).get(i));
+                                }
+                            }
+                            else
+                            {
+                                System.out.println("Il contenuto del file richiesto è stato trovato");
+                            }
 
-                    }
-                    else
-                    {
-                        System.out.println("Inserisci l'ID del nodo");
-                    }
-                    break;
-                case "printtree":
-                    System.out.println(myNode.printTree());
-                    break;
-                case "exit":
-                    System.exit(0);
-                    break;
-                default:
-                    System.out.println("Say what?");
-                    break;
+                        }
+                        else
+                        {
+                            System.out.println("Inserisci l'ID del nodo");
+                        }
+                        break;
+                    case "printtree":
+                        System.out.println(myNode.printTree());
+                        break;
+                    case "exit":
+                        System.exit(0);
+                        break;
+                    default:
+                        System.out.println("Say what?");
+                        break;
+                }
+            }
+
+            catch (ArrayIndexOutOfBoundsException aioobe)
+            {
+                System.out.println("Parametri non validi");
             }
         }
     }
